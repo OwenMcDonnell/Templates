5dc4ede74d43acb2b83c57b57a07541a3d7cc9bd
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-06-08 21:51:34 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -85,7 +85,7 @@ $TOOLS_DIR = Join-Path $PSScriptRoot "cake-tools"
 $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
 $CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
 $NUGET_URL = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-$PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
+$PACKAGES_CONFIG = "./packages.config"
 $PACKAGES_CONFIG_MD5 = Join-Path $TOOLS_DIR "packages.config.md5sum"
 
 # Should we use mono?
